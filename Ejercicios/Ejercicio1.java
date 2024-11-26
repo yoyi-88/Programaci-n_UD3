cdc755f201d676b31ee1825dcb8d44d146f5103d
yoyi-88 <ygomben2510@g.educaand.es>
2024-11-26 08:43:04 +0100

    act

---
+++ Ejercicios/Ejercicio1.java
@@ -0,0 +1,17 @@
+package Tema3;
+
+public class Ejercicio1 {
+	
+	public static void main(String[] args) {
+		
+		int [] Enteros = {1, 2, 3, 4, 5};
+		double [] Decimales = {0.5, 1.5, 2.5, 3.5, 4.5};
+		boolean [] Booleanos = {true, false};
+		
+		System.out.println(Enteros);
+		System.out.println(Decimales);
+		System.out.println(Booleanos);
+		
+	}
+
+}
---
+++ Ejercicios/Ejercicio2.java
@@ -0,0 +1,19 @@
+package Tema3;
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+		int [] tabla = new int [10];
+		tabla [0] = 1;
+		tabla [1] = 2;
+		tabla [2] = 3;
+		tabla [3] = 4;
+		tabla [4] = 5;
+		tabla [5] = 6;
+		tabla [6] = 7;
+		tabla [7] = 8;
+		tabla [8] = 9;
+		tabla [9] = 10;
+		System.out.println(tabla);
+	}
+
+}
